33efd3a67c7bb24e809b4ccac48d8dfeeeec67f8
Alex Kolch <alexkolch.ios@gmail.com>
2025-01-18 15:13:35 +0300

    Initial Commit

---
+++ BookSpecialist/BookSpecialistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSpecialistApp.swift
+//  BookSpecialist
+//
+//  Created by Алексей Колыченков on 18.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct BookSpecialistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
